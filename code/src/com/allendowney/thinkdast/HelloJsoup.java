e0c98e5d94b141e09de62f551f3fffec3e58e173
Adeildo Vieira <adeildo.vieira@duke.edu>
2025-12-13 15:20:25 -0500

    Refactor HelloJsoup class to improve createDocument method and update Java Wikipedia page content

--- code/src/com/allendowney/thinkdast/HelloJsoup.java
+++ code/src/com/allendowney/thinkdast/HelloJsoup.java
@@ -3,60 +3,32 @@ package com.allendowney.thinkdast;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.File;
-import java.io.IOException;
-
-/**
- * Example program to list links from a URL.
- * 
- * From: http://jsoup.org/cookbook/extracting-data/example-list-links
- */
-public class HelloJsoup {
-	
-	public static void main(String[] args) throws IOException {
-		
+		new HelloJsoup().createDocument();
+//		}
+//
+	private void createDocument() throws IOException {
 		String dirname = System.getProperty("user.dir");
 		String filename = "src/resources/en.wikipedia.org/wiki/Computer_science";
 		String baseURI = dirname + "/" + filename;
 
 		File input = new File(baseURI);
 		Document doc = Jsoup.parse(input, "UTF-8", baseURI);
-		
-		//print("Fetching %s...", url);
-		//Document doc = Jsoup.connect(url).get();
-		
+
 		Element content = doc.getElementById("mw-content-text");
+		if (content == null) {
+			print("Content not found in %s", filename);
+			return;
+		}
+
 		Elements paragraphs = content.getElementsByTag("p");
-		
-		
 		for (Element p : paragraphs) {
 			Elements links = p.select("a[href]");
 			for (Element link : links) {
 				print(" * a: <%s>  (%s)", link.attr("href"), trim(link.text(), 35));
 			}
+			// only display the first paragraph's links
 			break;
 		}
-
-		
-//		Elements media = doc.select("[src]");
-//		Elements imports = doc.select("link[href]");
-//
-//		print("\nMedia: (%d)", media.size());
-//		for (Element src : media) {
-//			if (src.tagName().equals("img")) {
-//				print(" * %s: <%s> %sx%s (%s)",
-//						src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
-//						trim(src.attr("alt"), 20));
-//			} else {	
-//				print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
-//			}
-//		}
-//
-//		print("\nImports: (%d)", imports.size());
-//		for (Element link : imports) {
-//			print(" * %s <%s> (%s)", link.tagName(),link.attr("abs:href"), link.attr("rel"));
 //		}
 //
 //		print("\nLinks: (%d)", links.size());
